66be5ac9b401689edf1dc3fb61c4d1a1a9d37b77
Hose9817 <abdullayev6859@gmail.com>
2021-07-24 08:23:15 +0600

    confirm for delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import axios from "axios";
+import ListItem from "./ListItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [list, setList] = useState([]);
+
+    const getList = () => {
+        axios.get('https://nazarov-kanban-server.herokuapp.com/card')
+            .then(res => {
+                console.log(res)
+                setList(res.data)
+            })
+            .catch(err => console.log(err))
+    }
+
+    const deleteList = async (id) => {
+        await axios.delete(`https://nazarov-kanban-server.herokuapp.com/card/${id}`)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+        getList()
+    }
+
+    const createCard = async () => {
+       await axios.post('https://nazarov-kanban-server.herokuapp.com/card', {
+            name: 'Deep words -',
+            description: 'Facta sunt potentiora verbis'
+        })
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+
+        getList();
+    }
+
+
+    return (
+        <div>
+
+            <h4>Axios</h4>
+
+            <button onClick={getList}>Get list</button>
+            <button onClick={createCard}>Create new card</button>
+           <hr/>
+
+            {list.map(el => <ListItem
+                key={el._id}
+                el={el}
+                deleteList={deleteList}
+            />)}
+        </div>
+    );
 }
+
 
 export default App;
